6c213631bbd26d8af4112ff213135485e08007b6
Jack Zhou <jackmkj@gmail.com>
2021-10-29 16:02:56 +1100

    update

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import './App.css';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import itemDetial from './ItemDetail';
 
 function App() {
-  
+
   return (
     <Router>
-    <div className="App">
-     <Nav/>
-     
-     <Route path="/about" component={About}/>
-     <Route path="/shop" component={Shop}/>
-    </div>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" exact component={Shop} />
+          <Route path="/shop/:id" component={itemDetial} />
+        </Switch>
+      </div>
     </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
+
 
 export default App;
--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import { Link } from 'react-router-dom';
 function Shop() {
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const [data, setData] = useState([]);
+
+  const fetchItems = async () => {
+    const data = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get');
+
+    const items = await data.json();
+    console.log(items.data);
+    setData(items.data);
+  };
   return (
     <div>
-     <h1>Shop</h1>
+      {data.map(data => (
+
+        <h1 key={data.itemId}>
+          <Link to={`/shop/${data.itemId}`}>{data.item.name}</Link>
+        </h1>
+
+      ))};
+
     </div>
   );
 }
